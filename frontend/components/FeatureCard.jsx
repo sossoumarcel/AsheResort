e0095e56ad023b7665c38c405be1edbb160f4eaf
Viridiana <Viridianaamoussou745@gmail.com>
2025-08-07 14:07:35 +0100

    Delete frontend/components/FeatureCard.jsx

--- frontend/components/FeatureCard.jsx
+++
@@ -1,9 +0,0 @@
-export const FeatureCard = ({ icon, title, description }) => {
-  return (
-    <div style={{ border: "1px solid #ccc", padding: "1rem", borderRadius: "8px" }}>
-      <div>{icon}</div>
-      <h3>{title}</h3>
-      <p>{description}</p>
-    </div>
-  );
-};
